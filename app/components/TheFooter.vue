21f4231f5c046bc865d6b6d94d7e6cfec83a05f6
Phillipp Bertram <phillipp.bertram@gmail.com>
2021-09-15 15:48:18 +0200

    center footer text; remove socials

--- app/components/TheFooter.vue
+++ app/components/TheFooter.vue
@@ -22,17 +22,10 @@
     <div class="footer-bottom black-bg">
       <div class="container">
         <div class="row">
-          <div class="col-md-5 order-2 order-md-1">
+          <div class="col text-center">
             <div class="copyright">
               <p>&copy; {{ copyrightYear }} <b class="text-white">Phillipp Bertram</b> · Made with <i
                   class="fa fa-heart text-danger"></i></p>
-            </div>
-          </div>
-          <div class="col-md-7 order-1 order-md-2">
-            <div class="social">
-              <a :href="social.link" target="_blank" v-for="social in data.socials" :key="social.id">
-                {{ social.text }}
-              </a>
             </div>
           </div>
         </div>
@@ -90,32 +83,6 @@
   @media #{$sm-device}{
     text-align: center;
     padding-top: 10px;
-  }
-}
-
-.social {
-  text-align: right;
-  // res
-  @media #{$sm-device}{
-    text-align: center;
-  }
-
-  a {
-    color: $paragraph-light;
-    line-height: 1;
-    margin-left: 30px;
-    // res
-    @media #{$md-device, $xxs-device}{
-      margin-left: 20px;
-    }
-
-    &:first-child {
-      margin-left: 0;
-    }
-
-    &:hover {
-      color: $theme-color-primary;
-    }
   }
 }
 
